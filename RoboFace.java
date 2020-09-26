2ee6ba012d8c0fd0341c871013db4ea374e9d264
Rocio Alvarez Bernal <dev.alvarezbernal@gmail.com>
2020-09-25 22:29:14 -0500

    started on roboface input const syntax with no value

--- RoboFace.java
+++ RoboFace.java
@@ -2,6 +2,22 @@
  * Decomp and Psuedo 
  * Make a face with the required specifications using acm graphics
  */
-public class RoboFace {
+
+import acm.graphics.*;
+import acm.program.*;
+
+public class RoboFace extends GraphicsProgram {
+// Constant Zone per assignment specifications 
+private static final int HEAD_WIDTH = ;
+private static final int HEAD_HEIGHT = ;
+private static final int EYE_RADIUS = ; 
+private static final int MOUTH_WIDTH = ;
+private static final int MOUTH_HEIGHT = ;
+	
+	public void run() {
+		
+	}
+	
+	
 
 }
