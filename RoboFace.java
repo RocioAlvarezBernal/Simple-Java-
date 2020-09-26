3cd79b6c120706285bfc0558b65cf82617b1136b
Rocio Alvarez Bernal <dev.alvarezbernal@gmail.com>
2020-09-26 00:11:30 -0500

    rectangle displaying as a square and not filled and not with demensions

--- RoboFace.java
+++ RoboFace.java
@@ -4,6 +4,8 @@
  * Predict I will need to use set add fill 
  */
 
+import java.awt.Color;
+
 import acm.graphics.*;
 import acm.program.*;
 
@@ -16,24 +18,29 @@ private static final int MOUTH_WIDTH = 40;
 private static final int MOUTH_HEIGHT = 20;
 	
 	public void run() {
+		head();
+//		add( head() );
 		
 	}
 
 //head should be centered gray with black outline 
 	private void head() {
-		HEAD_WIDTH
-		HEAD_HEIGHT
+		GRect headRect = new GRect(HEAD_WIDTH,HEAD_HEIGHT,100,100);
+		headRect.setFillColor(Color.GRAY);
+		
+		add(headRect);
+		
 	}
 /*pixels horizontally a quarter of the width of the head in from edges and one quarter of the distance down from
 the top of the head filled yellow.*/
 	private void eyes() {
-		EYE_RADIUS 
+//		EYE_RADIUS 
 		
 	}
 // centered in head x && 1/4 up from the bottom aka y dimension filled while 
 	private void mouth() {
-		MOUTH_WIDTH
-		MOUTH_HEIGHT
+//		MOUTH_WIDTH
+//		MOUTH_HEIGHT
 	}
  
 }
