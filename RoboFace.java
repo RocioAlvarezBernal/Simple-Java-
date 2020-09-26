00b0eaf17d8edad4f24600597448e9c8863090a6
Rocio Alvarez Bernal <dev.alvarezbernal@gmail.com>
2020-09-25 23:28:46 -0500

    added private methods for face feature with description

--- RoboFace.java
+++ RoboFace.java
@@ -1,6 +1,7 @@
 /*
  * Decomp and Psuedo 
  * Make a face with the required specifications using acm graphics
+ * Predict I will need to use set add fill 
  */
 
 import acm.graphics.*;
@@ -17,6 +18,22 @@ private static final int MOUTH_HEIGHT = 20;
 	public void run() {
 		
 	}
-	
 
+//head should be centered gray with black outline 
+	private void head() {
+		HEAD_WIDTH
+		HEAD_HEIGHT
+	}
+/*pixels horizontally a quarter of the width of the head in from edges and one quarter of the distance down from
+the top of the head filled yellow.*/
+	private void eyes() {
+		EYE_RADIUS 
+		
+	}
+// centered in head x && 1/4 up from the bottom aka y dimension filled while 
+	private void mouth() {
+		MOUTH_WIDTH
+		MOUTH_HEIGHT
+	}
+ 
 }
