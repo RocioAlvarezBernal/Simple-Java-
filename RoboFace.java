0b83abb1fb82b2d53b9687e6a34048c9cc3ddfac
Rocio Alvarez Bernal <dev.alvarezbernal@gmail.com>
2020-09-25 22:42:22 -0500

    took a guess at constant values they can be adjusted once they are added and seen in proportion to each other

--- RoboFace.java
+++ RoboFace.java
@@ -8,16 +8,15 @@ import acm.program.*;
 
 public class RoboFace extends GraphicsProgram {
 // Constant Zone per assignment specifications 
-private static final int HEAD_WIDTH = ;
-private static final int HEAD_HEIGHT = ;
-private static final int EYE_RADIUS = ; 
-private static final int MOUTH_WIDTH = ;
-private static final int MOUTH_HEIGHT = ;
+private static final int HEAD_WIDTH = 50;
+private static final int HEAD_HEIGHT = 100;
+private static final int EYE_RADIUS = 20; 
+private static final int MOUTH_WIDTH = 40;
+private static final int MOUTH_HEIGHT = 20;
 	
 	public void run() {
 		
 	}
-	
 	
 
 }
